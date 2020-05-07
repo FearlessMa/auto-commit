712c8281bea7257ccbd36037f5927ed572564ccd
mhc <mahaichao1030@qq.com>
2020-05-07 16:28:41 +0800

    feat: 测试

--- src/test.js
+++ src/test.js
@@ -1,1 +1,11 @@
 export const test = null;
+
+function a() {
+    console.log("a");
+    console.log("a");
+    console.log("a");
+    console.log("a");
+    console.log("a");
+}
+
+export default a;
