ccadccbdd59b196639b209f463c07aef9c9ab592
mhc <mahaichao1030@qq.com>
2020-05-08 17:27:27 +0800

    feat: ttt

---
+++ index.js
@@ -0,0 +1,11 @@
+export const test = null;
+
+function a() {
+    console.log("a");
+    console.log("a");
+    console.log("a");
+    console.log("a");
+    console.log("a");
+}
+
+export default a;
