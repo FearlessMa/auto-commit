ec629765be4b4d826cb661382f40096e86d247f6
mhc <mahaichao1030@qq.com>
2020-05-18 18:54:07 +0800

    feat: ttt

--- bin/index.js
+++ bin/index.js
@@ -231,7 +231,7 @@ inquirer.prompt(promptList).then(res => {
     shell.echo("开始执行git-cz：");
     infoBold(require(path.join(process.cwd(), binPath) + '/git-cz'))
     process.on('exit', function () {
-      const pullLoading = loading({ spinner: "" });
+      const pullLoading = loading();
       const pullMsg = exec("git pull");
       // shell.echo("\n pull：" + infoBold(pullMsg));
       pullLoading.succeed("\n pull：" + infoBold(pullMsg))
