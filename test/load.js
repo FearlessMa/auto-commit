1cf1d2f3c4c77ff7f3d0ce6ee1ca3b27a31ccd4a
mhc <mahaichao1030@qq.com>
2020-05-19 19:02:51 +0800

    test: 增加测试loading文件

--- test/load.js
+++ test/load.js
@@ -1,14 +1,38 @@
 const ora = require('ora');
-
+const shell = require('shelljs');
 const spinner = ora('Loading unicorns').start();
 
 spinner.color = 'blue';
 spinner.text = 'Loading rainbows';
 // 动画样式
 spinner.spinner = "weather";
-setTimeout(() => {
-  // spinner.stop();
+// shell.exec('sleep 2 && find load.js');
+// spinner.succeed();
+
+// setTimeout(() => {
+//   // spinner.stop();
+//   spinner.succeed();
+//   const r = spinner.isSpinning
+//   console.log('r: ', r);
+// }, 3000);
+
+let time = null;
+const load = () => {
+  const aminStr = '123456';
+  let i = 0;
+  time = setInterval(() => {
+    i = (++i) % aminStr.length;
+    process.stdout.write('\r' + aminStr[i])
+  }, 500)
+}
+// load();
+shell.exec('sleep 2 && find load.js',(code)=>{
+  console.log('code: ', code);
+  // clearInterval(time)
   spinner.succeed();
-  const r = spinner.isSpinning
-  console.log('r: ', r);
-}, 3000);
+
+});
+
+// setTimeout(() => {
+//   clearInterval(time)
+// }, 3000)
