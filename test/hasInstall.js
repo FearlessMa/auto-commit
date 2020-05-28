2e3c47a87ec6f81c620afffb9032edeea58023ae
mhc <mahaichao1030@qq.com>
2020-05-28 18:52:49 +0800

    test: ttt

--- test/hasInstall.js
+++ test/hasInstall.js
@@ -11,3 +11,9 @@ const has = hasInstall("react");
 console.log('has: ', has);
 const has1 = hasInstall("shelljs");
 console.log('has1: ', has1);
+
+
+// const child_process = require('child_process');
+// child_process.cwd()
+
+console.log(process.cwd())
